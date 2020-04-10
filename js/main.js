c5ff4d3c546132e5ee3c5cfe541e8f2bac22fa64
Muntasir Wahed <immuntasir@gmail.com>
2020-04-10 21:32:04 +0600

    data fetch fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function showInitialGraph(){
+function fetchDataAndLoadChart(){
     var data;
     urls = ["https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
             "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
@@ -15,16 +15,20 @@ function showInitialGraph(){
                 data = $.csv.toArrays(response);
                 allCountriesData[content_actual_name[i]] = csvJSON(data);
                 if (i==0) {
-                    showGraph(pr_country_name=chart_primary_country, countries=[], min_case_count = 10, init_day = 0, max_day = 30, content='cases',
-                    aggregation_over = chart_aggregation_over, aggregation_type=chart_aggregation_type, normalization='none', scale='linear');
-                    MakeDescription();
+                    fetchBangladeshLatestData();
                 }
                 }
             });
     }
 }
 
-function bringBangladeshLatestData(){
+function loadChart() {
+    showGraph(pr_country_name=chart_primary_country, countries=[], min_case_count = 10, init_day = 0, max_day = 30, content='cases',
+                    aggregation_over = chart_aggregation_over, aggregation_type=chart_aggregation_type, normalization='none', scale='linear');
+    MakeDescription();
+}
+
+function fetchBangladeshLatestData(){
   var data;
   data_url ='https://raw.githubusercontent.com/rizveeerprojects/Corona-History/master/bangladesh-data/time_series.csv';
   $.ajax({
@@ -40,13 +44,13 @@ function bringBangladeshLatestData(){
               bd_press_briefing_data['cases'] = parseInt(data[length-1][1]);
               bd_press_briefing_data['death'] =  parseInt(data[length-1][2]);
               bd_press_briefing_data['recovered'] =  parseInt(data[length-1][3]);
+              loadChart();
           }
       });
 }
 
 function initialize(){
-  showInitialGraph();
-  bringBangladeshLatestData();
+  fetchDataAndLoadChart();
 }
 
 $(document).ready(function(){
