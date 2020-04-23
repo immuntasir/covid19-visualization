d99068097be1affc03e2bd5aa2cedc414e93db10
Muntasir Wahed <immuntasir@gmail.com>
2020-04-24 03:14:19 +0600

    map legend hover

--- js/mapMaker.js
+++ js/mapMaker.js
@@ -69,27 +69,48 @@ function showDistributionMap () {
         map.setMaxBounds(map.getBounds());
 
 
-        legend.onAdd = function (map) {
+        legend.onAdd = function(map) {
             var div = L.DomUtil.create('div', 'info legend'),
                 grades = col_slices,
                 labels = [];
 
-
-
-            // loop through our density intervals and generate a label with a colored square for each interval
-
-            div.innerHTML +=
-                    '<i style="background: grey"></i> No Cases <br>' +
-                    '<i style="background:' + getColorByValue(grades[0] + 1) + '"></i> ' +
-                    grades[1]  + '<br>';
-            for (var i = 1; i < grades.length; i++) {
-                div.innerHTML +=
-                    '<i style="background:' + getColorByValue(grades[i] + 1) + '"></i> ' +
-                    (grades[i] + 1) + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+            
+            for (var i = 0; i < grades.length; i++) {
+              span = document.createElement("span");
+              span.classList.add("legendItem");
+              from = grades[i];
+              to = grades[i+1]
+              span.dataset.from = from;
+              span.dataset.to = to;
+          
+              color = document.createElement("i");
+              
+              if (i > 0) {
+                text = document.createTextNode(from + (to ? '-' + to : '+'));
+                color.style.background = getColorByValue(from + 1);
+              }
+              else if (i==0) {
+                  text = document.createTextNode('No Cases');
+                  color.style.background = 'grey';
+               }
+          
+              span.appendChild(color);
+              span.appendChild(text);
+          
+              span.addEventListener("mouseover", function (event) {
+                highlightMultipleFeatures(event);
+              });
+              
+              span.addEventListener("mouseout", function () {
+                bd_districts.eachLayer(resetHighlight);
+              });
+          
+              div.appendChild(span);
+              div.appendChild(document.createElement("br"));
             }
-
+          
             return div;
-        };
+          };
 
         legend.addTo(map);
 
@@ -115,6 +136,52 @@ function showDistributionMap () {
     }
     //var map = L.map('mapid').setView([51.505, -0.09], 13);
 
+}
+
+function highlightMultipleFeatures (event) {
+    var span = event.currentTarget,
+        from = span.dataset.from,
+        to = span.dataset.to,
+        above = to === "undefined";
+
+    bd_districts.eachLayer(function (layer) {
+        let highlighted = false;
+        if (from == '0' && to == '1') {
+        if (!district_data[layer.feature.properties['ADM2_EN']]) {
+            layer.setStyle({
+                weight: 2,
+                color: '#666',
+                dashArray: '',
+                fillOpacity: 1
+            });
+            if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                layer.bringToFront();
+            }
+            highlighted = true;
+        }
+        }
+        else if (district_data[layer.feature.properties['ADM2_EN']]) {
+        var cases = district_data[layer.feature.properties['ADM2_EN']][last_update];
+        if (cases >= parseInt(from) && (above || cases < parseInt(to))) {
+            layer.setStyle({
+                weight: 2,
+                color: '#666',
+                dashArray: '',
+                fillOpacity: 1
+            });
+            if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                layer.bringToFront();
+            }
+            highlighted = true;
+        }
+        }
+        if (!highlighted) {
+        layer.setStyle({
+            fillColor: 'white',
+            fillOpacity: 1
+        });
+        }
+    });
 }
 
 //#E97451, #D95D41, #C94631, #B92E20, #A91710,#990000
@@ -154,6 +221,32 @@ function style(feature) {
 
 function highlightFeature(e) {
     var layer = e.target;
+    bd_districts.resetStyle();
+
+    layer.setStyle({
+        weight: 2,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 1
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    if (district_data[layer.feature.properties['ADM2_EN']] != undefined) {
+        info.update(district_data[layer.feature.properties['ADM2_EN']]);
+    }
+    else {
+        let props = Object();
+        props['Alt_Name'] = layer.feature.properties['ADM2_EN'];
+        props[last_update] = 0;
+        info.update(props);
+    }
+}
+
+function highlightFeatureMultiple(e) {
+    var layer = e.target;
+    bd_districts.resetStyle();
 
     layer.setStyle({
         weight: 2,
