51dde1cb95e7f51afca1beaa79e09734b7977c51
Muntasir Wahed <immuntasir@gmail.com>
2020-06-05 10:09:48 +0600

    Revert "."
    This reverts commit 3a74ab305b0815198ca213b5c8d2802fd0d80178.

---
+++ js/main.js
@@ -0,0 +1,147 @@
+var latest_data;
+function fetchDataAndLoadChart(){
+    var data;
+    urls = ["https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
+            "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv",
+            "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"]
+
+    for (let i=0; i<urls.length; i++) {
+        data_url = urls[i];
+        $.ajax({
+            type: "GET",
+            url: data_url,
+            dataType: "text",
+            success: function(response)
+            {
+                data = $.csv.toArrays(response);
+                allCountriesData[content_actual_name[i]] = csvJSON(data);
+                if (i==2) {
+                    fetchBangladeshLatestData();
+                }
+                }
+            });
+    }
+}
+
+function loadChart() {
+    showGraph(pr_country_name=chart_primary_country, countries=[], min_case_count = 10, init_day = 0, max_day = 90, content='cases',
+                    aggregation_over = chart_aggregation_over, aggregation_type=chart_aggregation_type, normalization='none', scale='linear');
+    MakeDescription();
+}
+
+function initializeVariables() {
+    countries_to_compare.sort();
+    for (let i=0; i<countries_to_compare.length; i++) {
+        country_objects[countries_to_compare[i]] = Object();
+        country_objects[countries_to_compare[i]]['start_date'] = getStartDate(countries_to_compare[i], 1, 0, 30);
+        if (countries_to_compare[i] == 'Bangladesh') {
+            country_objects[countries_to_compare[i]]['color'] = "#006a4e";
+            country_objects[countries_to_compare[i]]['num_tests'] = Object();
+            country_objects[countries_to_compare[i]]['num_cases'] = Object();
+            country_objects[countries_to_compare[i]]['num_death'] = Object();
+            country_objects[countries_to_compare[i]]['num_recovered'] = Object();
+        }
+        else {
+            country_objects[countries_to_compare[i]]['color'] = country_colors[i];
+        }
+    }
+    genericSlider('valueSpan2','slider12');
+    initMinimumCaseSlider('valueSpan2');
+    genericSlider('valueSpan3','slider13');
+    showCountryOptions();
+    countrySelector();
+    addOnClickFunctions();
+    enablingToolip();
+
+
+    // Javascript to enable link to tab
+    var url = document.location.toString();
+    if (url.match('#')) {
+        let tab_name = url.split('#')[1];
+        if (tab_name != 'nav-list') {
+            $('.nav-tabs a[href="#' + tab_name + '"]').tab('show');
+        }
+    }
+
+    // Change hash for page-reload
+    $('.nav-tabs a').on('shown.bs.tab', function (e) {
+        window.location.hash = e.target.hash;
+        window.scrollTo(0, 0);
+    })
+
+
+    /***************Preset graph options *****************/
+    showCountryChartPreset();
+}
+
+function initializeCountryData (country_name='Bangladesh') {
+    for (let i=0; i<content_actual_name.length; i++) {
+        cur_country_data = getCountryRow(country_name, content_actual_name[i]);
+        let keys = Object.keys(cur_country_data).slice(4);
+        for (let j=0; j<keys.length; j++) {
+            country_objects[country_name]['num_' + content_actual_name[i]][keys[j]] = parseInt(cur_country_data[keys[j]]);
+        }
+    }
+    if (country_name == 'Bangladesh') {
+        for (let i=0; i<latest_data.length; i++) {
+            country_objects[country_name]['num_tests'][latest_data[i][0]] = parseInt(latest_data[i][4])
+        }
+    }
+}
+
+function fetchBangladeshLatestData(){
+  var data;
+  data_url ='https://raw.githubusercontent.com/rizveeerprojects/Corona-History/master/bangladesh-data/time_series.csv';
+  $.ajax({
+          type: "GET",
+          url: data_url,
+          dataType: "text",
+          success: function(response)
+          {
+
+              data = $.csv.toArrays(response);
+              latest_data = data;
+              let length = data.length;
+              bd_press_briefing_data['Date'] = data[length-1][0];
+              bd_press_briefing_data['num_cases'] = parseInt(data[length-1][1]);
+              bd_press_briefing_data['num_death'] =  parseInt(data[length-1][2]);
+              bd_press_briefing_data['num_recovered'] = parseInt(data[length-1][3]);
+              bd_press_briefing_data['num_test'] = parseInt(data[length-1][4]);
+
+              initializeVariables();
+              initializeCountryData('Bangladesh');
+              loadChart();
+              
+          }
+      });
+}
+
+function hideButtons(){
+  if(chart_aggregation_over=='cumulative'){
+    document.getElementById('dropdownMenuButton').style.display='none';
+  }
+}
+
+function dynamicContentNameGen(){
+  let keys = Object.keys(content_name_map);
+  let key,string='<div class="option-control">';
+  string += '<div class="dropdown">';
+  string += '<button class="btn btn-secondary btn-sm dropdown-toggle my-tooltip" type="button" id="dropdownMenuButton-content-name-map" data-toggle="dropdown" rel="tooltip" data-placement="top" title="Select an option" aria-haspopup="true" aria-expanded="false">';
+  string += '<span id="selected-aggregation-type-content-name-map">'+content_name_map[keys[0]]+'</span><span class="caret"></span>';
+  string += '</button>';
+  string += '<div class="dropdown-menu" id="dropdown-menu-aggregation-type-content-name-map" aria-labelledby="dropdownMenuButton-content-name-map">';
+  for(let i=0;i<keys.length;i++){
+    key='<a class="dropdown-item option-control-text" href="#" id="'+keys[i]+'">'+content_name_map[keys[i]]+'</a>'
+    string+=key;
+  }
+  string += '</div>';
+  string += '</div>';
+  string += '</div>';
+  $('#content-name-map-container').html(string);
+}
+
+function initialize(){
+    dynamicContentNameGen();
+    fetchDataAndLoadChart();
+    hideButtons();
+}
---
+++ js/mapMaker.js
@@ -0,0 +1,312 @@
+var bd_districts
+var mapLoaded = false;
+var district_data = Object();
+var last_update;
+var last_update_values = [];
+var col_slices = [];
+
+var legend = L.control({position: 'bottomright'});
+var info = L.control();
+
+
+function makeDistrictObjects (data) {
+    obj_keys = Object.keys(data[0]);
+    last_update = obj_keys.slice(-1, )
+    bd_map_render_date = last_update;
+    for (let data_iter in data) {
+        district_data[data[data_iter]['Alt_Name']] = Object();
+        for (let key_iter in obj_keys) {
+            district_data[data[data_iter]['Alt_Name']][obj_keys[key_iter]] = data[data_iter][obj_keys[key_iter]];
+        }
+        last_update_values.push(parseInt(data[data_iter][last_update]));
+    }
+
+    last_update_values = last_update_values.sort(function(a, b) {return a-b;})
+    col_slices = [0, 1, 10, 100, 1000];
+}
+
+function loadDataAndShowDistributionMap () {
+    if (mapLoaded){
+        return;
+    }
+    data_url ='https://raw.githubusercontent.com/rizveeerprojects/Corona-History/master/bangladesh-data/bd_cases.csv';
+    $.ajax({
+            type: "GET",
+            url: data_url,
+            dataType: "text",
+            success: function(response)
+            {
+                data = $.csv.toArrays(response);
+                data = csvJSON(data);
+                makeDistrictObjects(data);
+                showDistributionMap();
+                showLastUpdateDate();
+
+                showBangladeshDistrictWiseTable();
+                //bd_dates = Object.keys(district_data['Dhaka']).slice(3, );
+                //for (let i=0; i<bd_dates.length; i++) {
+                  //      reRenderBdMap(bd_dates[i]);
+                    //    sleep(100);
+                //}
+
+                showBangladeshDistrictWiseTable();
+                bdMapSliderInitiate(data[0]);
+
+            }
+        });
+}
+
+function showDistributionMap () {
+    if (mapLoaded == true) {
+        return;
+    }
+    var northEast = L.latLng(92.6727209818, 26.4465255803),
+        southWest = L.latLng(88.0844222351, 20.670883287),
+        bounds = L.latLngBounds(southWest, northEast);
+    if (map == undefined){
+        var map = L.map('mapid').setView([23.6850, 90.3563], 6);
+        var Wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+            attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
+            maxBounds: bounds,
+            minZoom: 6,
+            maxZoom: 19
+        });
+        Wikimedia.addTo(map);
+        bd_districts = new L.GeoJSON(bd_district_geojson, {
+            style: style,
+            onEachFeature: onEachFeature
+            });
+        bd_districts.addTo(map);
+        map.setMaxBounds(map.getBounds());
+
+
+        legend.onAdd = function(map) {
+            var div = L.DomUtil.create('div', 'info legend'),
+                grades = col_slices,
+                labels = [];
+
+
+            for (var i = 0; i < grades.length; i++) {
+              span = document.createElement("span");
+              span.classList.add("legendItem");
+              from = grades[i];
+              to = grades[i+1]
+              span.dataset.from = from;
+              span.dataset.to = to;
+
+              color = document.createElement("i");
+
+              if (i > 0) {
+                text = document.createTextNode(from + (to ? '-' + to : '+'));
+                color.style.background = getColorByValue(from + 1);
+              }
+              else if (i==0) {
+                  text = document.createTextNode('No Cases');
+                  color.style.background = 'grey';
+               }
+
+              span.appendChild(color);
+              span.appendChild(text);
+
+              span.addEventListener("mouseover", function (event) {
+                highlightMultipleFeatures(event);
+              });
+
+              span.addEventListener("mouseout", function () {
+                bd_districts.eachLayer(resetHighlight);
+              });
+
+              div.appendChild(span);
+              div.appendChild(document.createElement("br"));
+            }
+
+            return div;
+          };
+
+        legend.addTo(map);
+
+
+
+        info.onAdd = function (map) {
+            this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+            this.update();
+            return this._div;
+        };
+
+        // method that we will use to update the control based on feature properties passed
+        info.update = function (props) {
+            this._div.innerHTML = '<h4>Number of COVID-19 Cases</h4>' +  (props ?
+                '<b>' + props['Alt_Name'] + '</b><br />' +'Total Cases: ' + props[bd_map_render_date]
+                : 'Hover over a district');
+        };
+
+        info.addTo(map);
+
+
+        mapLoaded = true;
+    }
+    //var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+}
+
+function reRenderBdMap (date) {
+    bd_map_render_date = date;
+    bd_districts.eachLayer(function (layer) {
+        layer.setStyle({
+            fillColor: getColor(layer.feature.properties),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '1',
+            fillOpacity: 1
+        });
+    });
+}
+
+function highlightMultipleFeatures (event) {
+    var span = event.currentTarget,
+        from = span.dataset.from,
+        to = span.dataset.to,
+        above = to === "undefined";
+
+    bd_districts.eachLayer(function (layer) {
+        let highlighted = false;
+        if (from == '0' && to == '1') {
+        if (!district_data[layer.feature.properties['ADM2_EN']]) {
+            layer.setStyle({
+                weight: 2,
+                color: '#666',
+                dashArray: '',
+                fillOpacity: 1
+            });
+            if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                layer.bringToFront();
+            }
+            highlighted = true;
+        }
+        }
+        else if (district_data[layer.feature.properties['ADM2_EN']]) {
+        var cases = district_data[layer.feature.properties['ADM2_EN']][bd_map_render_date];
+        if (cases >= parseInt(from) && (above || cases < parseInt(to))) {
+            layer.setStyle({
+                weight: 2,
+                color: '#666',
+                dashArray: '',
+                fillOpacity: 1
+            });
+            if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                layer.bringToFront();
+            }
+            highlighted = true;
+        }
+        }
+        if (!highlighted) {
+        layer.setStyle({
+            fillColor: 'white',
+            fillOpacity: 1
+        });
+        }
+    });
+}
+
+//#E97451, #D95D41, #C94631, #B92E20, #A91710,#990000
+//#E31A1C, FF0000
+
+function getColorByValue (comp_val) {
+    return comp_val > col_slices[4] ? '#800026' :
+           comp_val > col_slices[3] ? '#FF0000' :
+            comp_val > col_slices[2]  ? '#FF9650' :
+            comp_val > col_slices[1]   ? '#FFBA6A' :
+            comp_val > col_slices[0]   ? '#FFD785' :
+                        '#FFEDA0';
+}
+
+function getColor(d) {
+    if (d['ADM2_EN'] in district_data) {
+        let comp_val =  district_data[d['ADM2_EN']][bd_map_render_date];
+        return getColorByValue(comp_val);
+    }
+    else {
+        return 'grey';
+    }
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '1',
+        fillOpacity: 1
+    };
+}
+
+
+
+function highlightFeature(e) {
+    var layer = e.target;
+    bd_districts.resetStyle();
+
+    layer.setStyle({
+        weight: 2,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 1
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    if (district_data[layer.feature.properties['ADM2_EN']] != undefined) {
+        info.update(district_data[layer.feature.properties['ADM2_EN']]);
+    }
+    else {
+        let props = Object();
+        props['Alt_Name'] = layer.feature.properties['ADM2_EN'];
+        props[bd_map_render_date] = 0;
+        info.update(props);
+    }
+}
+
+function highlightFeatureMultiple(e) {
+    var layer = e.target;
+    bd_districts.resetStyle();
+
+    layer.setStyle({
+        weight: 2,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 1
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    if (district_data[layer.feature.properties['ADM2_EN']] != undefined) {
+        info.update(district_data[layer.feature.properties['ADM2_EN']]);
+    }
+    else {
+        let props = Object();
+        props['Alt_Name'] = layer.feature.properties['ADM2_EN'];
+        props[bd_map_render_date] = 0;
+        info.update(props);
+    }
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function resetHighlight(e) {
+    bd_districts.resetStyle(e.target);
+    info.update();
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: highlightFeature
+    });
+}
